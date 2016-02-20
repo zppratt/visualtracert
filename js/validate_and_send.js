bb562b7f31117078b3424567b0bea95ec8adb7f5
Zach Pratt <zppratt+github@gmail.com>
2016-02-19 20:20:13 -0500

    Various small tweaks

--- js/validate_and_send.js
+++ js/validate_and_send.js
@@ -1,9 +1,9 @@
 function ValidateIPaddress(ipaddress) {
     if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress)) {
-        return (true)
+        return (true);
     }
-    alert("You have entered an invalid IP address!")
-    return (false)
+    alert("You have entered an invalid IP address!");
+    return (false);
 }
 
 function sendTracerouteRequest(ipaddress) {
@@ -18,6 +18,7 @@ function sendTracerouteRequest(ipaddress) {
 	httpRequest.open("POST", "traceroute.php", true);
 	httpRequest.setRequestHeader("Content-type", "application/json");
 	httpRequest.send(JSON.stringify(ipaddress));
+    	alert("Sent!");
 
 	console.log(JSON.stringify(ipaddress)) // Just in case for debugging, will remove later 
 }
