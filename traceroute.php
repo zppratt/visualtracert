d0c60f0df37040379bc402aa0c10825af8212e98
SunnyDjinn <guiol.matt@gmail.com>
2016-02-24 10:23:58 -0500

    Refactored traceroute.php

--- traceroute.php
+++ traceroute.php
@@ -1,43 +1,66 @@
 <?php
 
+/* 
+ * Ensures that the IP sent by the client is in the correct format and returns the ip address extracted
+ */
+function validateClientRequest($request) {
+	/* Decoding the JSON object in a php array */
+	$ipAddress = json_decode($request, true);
+
+	/* ipAddress is supposed to have the right format, but we check on the server side again */
+	$ipAddress = filter_var($ipAddress, FILTER_VALIDATE_IP);
+
+	if($ipAddress == FALSE) { // couldn't filter an ip address, therefore the format wasn't correct
+		exit(json_encode("Bad IP address format. Aborting."));
+	}
+
+	return $ipAddress;
+}
+
+/* 
+ * Parses the results of traceroute and returns an array of valid ip addresses for the hops
+ */
+function parseTraceroute($tracerouteOutput) {
+	$HopsIpAddresses = array();	// Array of future valid ip addresses
+
+	for($i=1; $i<count($tracerouteOutput); $i++){	// Scouring all responses from traceroute (except the first line that we know is just descriptive)
+
+		$exploded = explode(" ", $tracerouteOutput[$i]);
+
+		foreach($exploded as $potentialIp) {
+			if (filter_var($potentialIp, FILTER_VALIDATE_IP) == TRUE) // We have the first match of ip address in the response line
+				break;
+		}
+		array_push($HopsIpAddresses, $potentialIp);
+	}
+
+	return $HopsIpAddresses;
+}
+
+/* 
+ * Execute the traceroute call and returns the IP addresses of the hops
+ * TODO: make sure exec() finishes its execution and take care of the timeout
+ */
+function executeTraceroute($ipAddress) {
+	$returnValue;
+	$tracerouteOutput = array();
+
+	exec("traceroute ".$ipAddress, $tracerouteOutput, $returnValue); // execute the traceroute 
+
+	$HopsIpAddresses = parseTraceroute($tracerouteOutput);
+
+	return $HopsIpAddresses;
+}
+
+
 /* Getting contents from JSON string sent */
 $requestReceived = file_get_contents('php://input');
 
-/* Decoding the JSON object in a php array */
-$ipAddress = json_decode($requestReceived, true);
+$ipAddress = validateClientRequest($requestReceived);
 
-/* ipAddress is supposed to have the right format, but we check on the server side again */
-$ipAddress = filter_var($ipAddress, FILTER_VALIDATE_IP);
+$HopsIpAddresses = executeTraceroute($ipAddress);
 
-if($ipAddress == FALSE) { // couldn't filter an ip address, therefore the format wasn't correct
-	exit(json_encode("Bad IP address format. Aborting."));
-}
-
-
-$returnValue;
-$tracerouteOutput = array();
-
-/* 
-	Is exec the best solution? Need to be parsed to retrieve only IP addresses
-	TODO: Find a way to make it asynchronous so we can look for geolocation and update the client page as traceroute is running
-
-*/
-exec("traceroute ".$ipAddress, $tracerouteOutput, $returnValue); // execute the traceroute 
-
-$HopsIpAddresses = array();	// Array of future valid ip addresses
-
-for($i=1; $i<count($tracerouteOutput); $i++){	// Scouring all responses from traceroute (except the first line that we know is just descriptive)
-
-	$exploded = explode(" ", $tracerouteOutput[$i]);
-
-	foreach($exploded as $potentialIp) {
-		if (filter_var($potentialIp, FILTER_VALIDATE_IP) == TRUE) // We have the first match of ip address in the response line
-			break;
-	}
-	array_push($HopsIpAddresses, $potentialIp);
-}
-
-echo json_encode($HopsIpAddresses);
+echo json_encode($HopsIpAddresses);	// sends to client for now
 
 //echo json_encode($tracerouteOutput);
 
@@ -45,15 +68,6 @@ echo json_encode($HopsIpAddresses);
 // was previously for the use of tracerouteimpl
 //require('tracerouteimpl.php');
 //$tracerouteResults = traceroute($ipAddress);
-
-
-// send to client 
-//echo json_encode($tracerouteResults[0]);
-
-/*foreach($tracerouteOutput as $stringValue) {
-	echo $stringValue . '<br>';
-}*/
-
 
 
 ?>
