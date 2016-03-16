a95d61586989f564fc33b417456ccc0472e0be54
SunnyDjinn <guiol.seb@gmail.com>
2016-03-16 08:06:14 -0400

    Modified geocoding fetching to reduce the possibility of OVER_QUERY_LIMIT

--- js/map.js
+++ js/map.js
@@ -1,3 +1,7 @@
+
+/*
+ * Initializes a blank map (nothing drawn on it) in the HTML page 
+ */
 function initialize() {
     var mapProp = {
         zoom: 4,
@@ -5,31 +9,6 @@ function initialize() {
         center: {lat: 28.540, lng: -100.546}
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-    var geocoder = new google.maps.Geocoder();
-    var addresses = [ "Fort Wayne, Indiana", "Denver, CO", "San Francisco, Ca" ];
-    var flightPath;
-    var myTrip = [];
-    
-    for (var i=0; i < addresses.length; i++) {
-        geocoder.geocode({
-            'address' : addresses[i]
-        }, function(geocode_results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
-                myTrip.push({
-                    lat : geocode_results[0].geometry.location.lat(),
-                    lng : geocode_results[0].geometry.location.lng()
-                });
-                console.log(myTrip);
-                flightPath = new google.maps.Polyline({
-                    path : myTrip,
-                    strokeColor : "#0000FF",
-                    strokeWeight : 2,
-                    strokeOpacity : 0.8,
-                    map : map
-                });
-            }
-        });
-    }
 }
 
 /* 
@@ -47,10 +26,26 @@ function plotOnMap(serverResponse){
 	var myTrip = [];
 
     for (var i=0; i < serverResponse.length; i++) {
+    	/* 
+    	 * Terrible code but only temporary - for testing purposes 
+    	 * Ensures that we don't get any OVER_QUERY_LIMIT response and can actually display every point on the map
+    	 * Some points are not displayed when too many queries - doesn't retry automatically 
+    	 */
+    	if(i != 0 && serverResponse[i].streetAddress == serverResponse[i-1].streetAddress && serverResponse[i].city == serverResponse[i-1].city)
+    		continue;
+
+    	/*
+		 * The geocoder's callback function makes the plotting out f order - and it matters for polylines
+		 * Consider Reordering "my trip" each time so we keep a consistent path
+    	 */
         geocoder.geocode({
-            'address' : serverResponse[i].city + ", " + serverResponse[i].state + ", " + serverResponse[i].postalCode + ", " + serverResponse[i].country
+            'address' : /*serverResponse[i].streetAddress + ", " + */serverResponse[i].city + ", " + serverResponse[i].state + " " 
+            + serverResponse[i].postalCode + ", " + serverResponse[i].country // Street address returns sometimes no results -- Disabled for presentation
         }, function(geocode_results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
+        	if (status != google.maps.GeocoderStatus.OK) {
+        		console.log(status);
+        	}
+            else if (status == google.maps.GeocoderStatus.OK) {
                 myTrip.push({
                     lat : geocode_results[0].geometry.location.lat(),
                     lng : geocode_results[0].geometry.location.lng()
@@ -66,5 +61,4 @@ function plotOnMap(serverResponse){
             }
         });
     }
-
 }
